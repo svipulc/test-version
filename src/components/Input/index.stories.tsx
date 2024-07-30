e8a0dadfcb186b48e3a91e992c4ac354e4728724
svipulc <vipul.c@simformsolutions.com>
2024-07-30 15:32:27 +0530

    feat: add input component

---
+++ src/components/Input/index.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Input } from ".";
+
+const meta: Meta<typeof Input> = {
+  title: "Components/Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Solid: Story = {
+  args: {},
+};
