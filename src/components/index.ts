f4df846b588a131b333e996049c01ebcbd7f0c6f
svipulc <vipul.c@simformsolutions.com>
2024-07-31 15:51:29 +0530

    Publish configuration

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export * from "./Avatar";
 export * from "./Button";
 export * from "./Input";
 export * from "./card";
-export * from "./Label"
+export * from "./Label";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/index.ts"),
+      name: "YourLibraryName",
+      formats: ["es", "umd"],
+      fileName: (format) => `your-library.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
